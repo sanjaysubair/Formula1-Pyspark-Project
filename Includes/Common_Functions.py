1e073ab51e5bccd46363975657e1e79340684e41
sanjaysubair <47313401+sanjaysubair@users.noreply.github.com>
2024-01-09 23:47:28 +0530

    Update Common_Functions.py

--- Includes/Common_Functions.py
+++ Includes/Common_Functions.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+#Created a notebook with a function, which inputs a dataframe, and returns a dataframe with the ingestion_time column added.
 from pyspark.sql.functions import current_timestamp
 def add_ingestion_date(input_df):
     output_df = input_df.withColumn("Ingestion_Time", current_timestamp())
