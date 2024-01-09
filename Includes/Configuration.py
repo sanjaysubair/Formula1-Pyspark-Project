4eaaf8d4bd6cf8826edf909a39c9312e1394612e
sanjaysubair <47313401+sanjaysubair@users.noreply.github.com>
2024-01-09 23:47:01 +0530

    Update Configuration.py

--- Includes/Configuration.py
+++ Includes/Configuration.py
@@ -1,4 +1,6 @@
 # Databricks notebook source
+#Created a notebook with variables for raw, processed and presentation container URL, 
+#and replaced the hardcoded urls using f-string method in the Ingest_circuits_csv file.
 raw_folder_path = "abfss://raw@sanjayformula1dl2.dfs.core.windows.net"
 processed_folder_path = "abfss://processed@sanjayformula1dl2.dfs.core.windows.net"
 presentation_folder_path = "abfss://presentation@sanjayformula1dl2.dfs.core.windows.net"
